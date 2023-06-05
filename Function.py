4d49936c5d234e3db4b9335ba50da8a7d070b9a6
leonhaffner <leon.haffner@gmail.com>
2023-06-05 16:17:22 +0200

    Function seg_orientation_lines compromised in one Function

--- Function.py
+++ Function.py
@@ -46,11 +46,13 @@ def find_thresh (image, persentage):
     h = image.shape[1]
     all_grayscale_values = []
     #x_values = []
-
+    
     for y in range(int(h/2), int(h-1), int(h/8)):
         gray_values, x_values = grayscale_values(image, y)
         all_grayscale_values.extend(gray_values)
-
+        
+    
+    print(all_grayscale_values)
     max_gray = max(all_grayscale_values)
     
     sorted_values = sorted(all_grayscale_values)  # Sortiere die Grauwerte aufsteigend
@@ -113,11 +115,139 @@ def seg_largest_component(image):
     return img_out
 
 #%%
+# def seg_orientation_lines(image, color):
+#     '''
+#     segmentiert aus einem Imput Bild die Ortientierungslinien
+#     Output: bw_image with contours of orientation lines
+#     '''
+
+#     def find_largest_component (image):
+#         '''
+#         find all connected Components, search for the largest
+#         Output: Image with the largest Component
+#         '''
+#         # Finde alle zusammenhängenden Elemente
+#         num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(image)
+
+#         # Finde das größte zusammenhängende Element
+#         largest_component_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
+
+#         # Erstelle ein Bild, das nur das größte zusammenhängende Element enthält
+#         largest_component_image = np.zeros_like(image)
+#         largest_component_image[labels == largest_component_label] = 255
+
+#         return largest_component_image
+    
+
+#     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     plt.gray()
+
+#     #Gauss für bessere Ergebnisse bei finde thresh
+#     img_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+
+#     #color = "w"   #Farbe der Orientierungslinie wählen: w für weiß und s für schwarz
+#     #img_b_or_w = white_or_black(img_blur, color)
+#     if color == "w":
+#         img_b_or_w = image
+#     else:
+#         if color == "s":
+#             img_b_or_w = 255 - image
+#         else:
+#             raise ValueError("Ungültige Farbauswahl. Gültige Optionen sind s oder w.")
+
+#     #thresh_1, thresh_2 = find_thresh(img_b_or_w, 0.13)  # oberen möglichen 13% ab maximalem Grauwert
+#     h = img_b_or_w.shape[1]
+#     all_grayscale_values = []
+    
+#     for y in range(int(h/2), int(h-1), int(h/8)):
+#         gray_values, x_values = grayscale_values(img_b_or_w, y)
+#         all_grayscale_values.extend(gray_values)
+
+#     max_gray = max(all_grayscale_values)
+    
+#     sorted_values = sorted(all_grayscale_values)  # Sortiere die Grauwerte aufsteigend
+#     num_values = len(sorted_values)  # Anzahl der Grauwerte
+
+#     # oberen möglichen 13% ab maximum Grauwert
+#     persantage = 0.13
+#     thresh_percent = 1-persantage
+    
+#     index_1 = int(num_values * thresh_percent)  # Index für den 13% Punkt
+#     threshold_2 = int(max_gray - (max_gray*persantage))
+
+#     #print(index_1)
+#     threshold_1 = sorted_values[index_1]
+
+
+#     print("1:", threshold_1)
+#     print("2:", threshold_2)
+
+#     t, seg = cv2.threshold(img_b_or_w,threshold_2,255,cv2.THRESH_BINARY)
+
+#     #segmentiert das größe zusammenhängende Objekt
+#     # Erstelle ein Bild, das nur das größte zusammenhängende Element enthält
+#     #img_out = seg_largest_component(seg)
+#     img_dilate = cv2.dilate(seg.astype('uint8'), np.ones((3,3)), iterations=7)
+#     #plt.imshow(img_dilate)
+
+#     img_largest = find_largest_component(img_dilate)
+#     #plt.imshow(img_largest)
+
+#     #Um evtl. falsch verbundene Komponenten zu entfernen nocheinmal erode, find_largest_component, dilate
+#     iterations_2 = 8
+
+#     img_erode = cv2.erode(img_largest.astype('uint8'), np.ones((3,3)), iterations=iterations_2)
+#     #plt.imshow(img_erode)
+
+#     img_largest_2 = find_largest_component(img_erode)
+#     #plt.imshow(img_largest_2)
+
+#     img_out = cv2.dilate(img_largest_2.astype('uint8'), np.ones((3,3)), iterations= iterations_2*2)
+
+#     return img_out
+
+
+#%%
 def seg_orientation_lines(image, color):
     '''
     segmentiert aus einem Imput Bild die Ortientierungslinien
     Output: bw_image with contours of orientation lines
     '''
+
+    def grayscale_values(image, y_position):
+        # Linienbild erstellen
+        #line_image = create_horizontal_line(image, y_position)
+
+        # x-Achse und Grauwerte initialisieren
+        x_values = np.arange(image.shape[1])
+        gray_values = []
+
+        # Grauwerte entlang der Linie sammeln
+        for x in x_values:
+            gray_values.append(image[y_position, x])
+
+        return gray_values, x_values
+    
+
+    def find_largest_component (image):
+        '''
+        find all connected Components, search for the largest
+        Output: Image with the largest Component
+        '''
+        # Finde alle zusammenhängenden Elemente
+        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(image)
+
+        # Finde das größte zusammenhängende Element
+        largest_component_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
+
+        # Erstelle ein Bild, das nur das größte zusammenhängende Element enthält
+        largest_component_image = np.zeros_like(image)
+        largest_component_image[labels == largest_component_label] = 255
+
+        return largest_component_image
+    
+
+
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     plt.gray()
 
@@ -125,17 +255,63 @@ def seg_orientation_lines(image, color):
     img_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
 
     #color = "w"   #Farbe der Orientierungslinie wählen: w für weiß und s für schwarz
-    img_b_or_w = white_or_black(img_blur, color)
+    #img_b_or_w = white_or_black(img_blur, color)
+    if color == "W":
+        img_b_or_w = img_blur
+    else:
+        if color == "B":
+            img_b_or_w = 255 - img_blur
+        else:
+            raise ValueError("Invalid color selection. Valid options are B for Black or W for White")
 
-    thresh_1, thresh_2 = find_thresh(img_b_or_w, 0.13)  # oberen möglichen 13% ab maximalem Grauwert
+    #thresh_1, thresh_2 = find_thresh(img_b_or_w, 0.13)  # oberen möglichen 13% ab maximalem Grauwert
+    h = img_b_or_w.shape[1]
+    all_grayscale_values = []
+    
+    for y in range(int(h/2), int(h-1), int(h/16)):
+        gray_values, x_values = grayscale_values(img_b_or_w, y)
+        all_grayscale_values.extend(gray_values)
 
-    print("1:", thresh_1)
-    print("2:", thresh_2)
+    print(all_grayscale_values)
+    max_gray = max(all_grayscale_values)
+    
+    sorted_values = sorted(all_grayscale_values)  # Sortiere die Grauwerte aufsteigend
+    num_values = len(sorted_values)  # Anzahl der Grauwerte
 
-    t, seg = cv2.threshold(img_b_or_w,thresh_2,255,cv2.THRESH_BINARY)
+    # oberen möglichen 13% ab maximum Grauwert
+    persantage = 0.13
+    thresh_percent = 1-persantage
+    
+    index_1 = int(num_values * thresh_percent)  # Index für den 13% Punkt
+    threshold_2 = int(max_gray - (max_gray*persantage))
 
-    #segmentiert das größe zusammenhängende Objekt 
-    img_out = seg_largest_component(seg)
+    #print(index_1)
+    threshold_1 = sorted_values[index_1]
+
+
+    print("1:", threshold_1)
+    print("2:", threshold_2)
+
+    t, seg = cv2.threshold(img_b_or_w,threshold_2,255,cv2.THRESH_BINARY)
+
+    #segmentiert das größe zusammenhängende Objekt
+    # Erstelle ein Bild, das nur das größte zusammenhängende Element enthält
+    img_dilate = cv2.dilate(seg.astype('uint8'), np.ones((3,3)), iterations=7)
+    #plt.imshow(img_dilate)
+
+    img_largest = find_largest_component(img_dilate)
+    #plt.imshow(img_largest)
+
+    #Um evtl. falsch verbundene Komponenten zu entfernen nocheinmal erode, find_largest_component, dilate
+    iterations_2 = 8
+
+    img_erode = cv2.erode(img_largest.astype('uint8'), np.ones((3,3)), iterations=iterations_2)
+    #plt.imshow(img_erode)
+
+    img_largest_2 = find_largest_component(img_erode)
+    #plt.imshow(img_largest_2)
+
+    img_out = cv2.dilate(img_largest_2.astype('uint8'), np.ones((3,3)), iterations= iterations_2*2)
 
     return img_out
 
