dcbba91ec4f13db0e77270cf56ee7f98e89903dd
leonhaffner <leon.haffner@gmail.com>
2023-06-16 20:09:28 +0200

    new version copied from main branch

---
+++ tools_1.py
@@ -0,0 +1,32 @@
+"""here we are collection our tools"""
+
+import cv2
+
+def read_video_frames(video_path):
+    """function to read in video frame by frame and save it in a list with an index"""
+    # Create a VideoCapture object
+    cap = cv2.VideoCapture(video_path)
+
+    # Check if the video capture is successfully opened
+    if not cap.isOpened():
+        print("Error opening video file.")
+        return
+
+    frame_list = []  # List to store the frames
+    index = 0
+
+    while cap.isOpened():
+        # Read the next frame
+        ret, frame = cap.read()
+
+        # If the frame is not successfully read, the video has ended
+        if not ret:
+            break
+
+        frame_list.append((index, frame))  # Append the frame along with its index
+        index += 1
+
+    # Release the VideoCapture object
+    cap.release()
+
+    return frame_list
